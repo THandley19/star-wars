d442e4a277802f4c68d815dfa517c5bb5176df93
THandley19 <trevor.handley@gmail.com>
2021-08-13 11:11:45 -0400

    initial struc of app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserCharacter from "./compoments/User-Character/index";
+
+function App() {
+  return (
+    <div className="App">
+      <UserCharacter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/compoments/User-Character/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function UserCharacter() {
+  return (
+    <div>
+      <div id="main-container">
+        <div className="character-card">Hello</div>
+        <div className="character-card">Hello</div>
+      </div>
+    </div>
+  );
+}
